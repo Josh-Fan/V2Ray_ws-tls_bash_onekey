5ec2d61f05b9e228222772c90b14db86e591969f
Josh-Fan <45245950+Josh-Fan@users.noreply.github.com>
2019-12-01 18:38:44 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -106,8 +106,9 @@ chrony_install(){
     chronyc sourcestats -v
     chronyc tracking -v
     date
-    read -p "请确认时间是否准确,误差范围±3分钟(Y/N): " chrony_install
-    sleep 10
+    #read -p "请确认时间是否准确,误差范围±3分钟(Y/N): " chrony_install
+    echo "请确认时间是否准确,误差范围±3分钟(Y/N): "
+    read chrony_install;
     [[ -z ${chrony_install} ]] && chrony_install="Y"
     case $chrony_install in
         [yY][eE][sS]|[yY])
